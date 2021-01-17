7008ffb304372055d548d350db00f76b0fb1fd46
chinmay padhi <pahichinmay1998@gmail.com>
2021-01-17 17:12:00 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="dropdown_in_asp.net.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript">
+        var x = () => {
+            var s = document.getElementById("DropDownList1").value;
+            if (s == 1) {
+                alert("plz enter any value without 1");
+            }
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList  ID="DropDownList1" runat="server" >
+            </asp:DropDownList>
+            <asp:Button ID="Button1" runat="server" Text="Button" />
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace dropdown_in_asp.net
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("server=.;database=master;integrated security=true");
+        SqlCommand cmd;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                if(con.State==ConnectionState.Closed)
+                {
+                    con.Open();
+                    cmd = con.CreateCommand();
+                    cmd.CommandText = "select *from chinmay";
+                    cmd.Connection = con;
+                    con.Close();
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                    adp.Fill(dt);
+                    DropDownList1.DataSource = dt;
+                    DropDownList1.DataBind();
+                    DropDownList1.DataTextField = "ch_id";
+                    DropDownList1.DataValueField = "ch_id";
+                    DropDownList1.DataBind();
+
+                }
+
+            }
+
+        }
+    }
+}
